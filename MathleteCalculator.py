9a3e8c6be46d607774a85337c459825cda5914b6
abblinsun <92145470+abblinsun@users.noreply.github.com>
2022-09-09 19:59:07 +0200

    Add files via upload

---
+++ MathleteCalculator.py
@@ -0,0 +1,99 @@
+import os
+def cls():
+    os.system('cls' if os.name=='nt' else 'clear')
+
+while True:
+    print("_________________________\n")
+    print("Mathlete Calculator")
+    print("_________________________")
+    print("add | Add two numbers")
+    print("sub | Subtract two numbers")
+    print("mul | Multiply two numbers")
+    print("div | divide two numbers")
+    print("__________________________")
+    op = input("Selection >  ")
+    if op.lower() == "add":
+        print("_______________________")
+        print("calculating 'c' for expression:\n")
+        print("     a + b = c\n")
+        print("please enter value for 'a' and 'b'.\n")
+        a = input("a = ")
+        b = input("b = ")
+        print()
+        try:
+            c = float(a) + float(b)
+            print()
+            if c == int(c):
+                c = int(c)
+            print("Result: " + str(a) + " + " + str(b) + " = " + str(c) + "\n") 
+            input("Press enter to continue...")
+            cls()
+        except ValueError:
+            print("ERROR: invalid expression ' " + str(a) + " + " + str(b) + " '\n")
+            input("press enter to continue...")
+
+    elif op.lower() == "sub":
+        print("_______________________")
+        print("calculating 'c' for expression:\n")
+        print("     a - b = c\n")
+        print("please enter value for 'a' and 'b'.\n")
+        a = input("a = ")
+        b = input("b = ")
+        print()
+        try:
+            c = float(a) - float(b)
+            print()
+            if c == int(c):
+                c = int(c)
+            print("Result: " + str(a) + " - " + str(b) + " = " + str(c) + "\n")
+            input("Press enter to continue...")
+            cls()
+        except ValueError:
+            print("ERROR: invalid expression ' " + str(a) + " - " + str(b) + " '\n")
+            input("press enter to continue...")
+    elif op.lower() == "mul":
+        print("_______________________")
+        print("calculating 'c' for expression:\n")
+        print("     a * b = c\n")
+        print("please enter value for 'a' and 'b'.\n")
+        a = input("a = ")
+        b = input("b = ")
+        print()
+        try:
+            c = float(a) * float(b)
+            print()
+            if c == int(c):
+                c = int(c)
+            print("Result: " + str(a) + " * " + str(b) + " = " + str(c) + "\n")
+            input("Press enter to continue...")
+            cls()
+        except ValueError:
+            print("ERROR: invalid expression ' " + str(a) + " * " + str(b) + " '\n")
+            input("press enter to continue...")
+    elif op.lower() == "div":
+        print("_______________________")
+        print("calculating 'c' for expression:\n")
+        print("     a / b = c\n")
+        print("please enter value for 'a' and 'b'.\n")
+        a = input("a = ")
+        b = input("b = ")
+        print()
+        try:
+            c = float(a) / float(b)
+            if c == int(c):
+                c = int(c)
+            print()
+            print("Result: " + str(a) + " / " + str(b) + " = " + str(c) +"\n")
+            input("Press enter to continue...")
+            cls()
+        except ValueError:
+            print("ERROR: invalid expression ' " +
+                  str(a) + " / " + str(b) + " '\n")
+            input("press enter to continue...")
+    elif op == "":
+        cls()   
+    else:
+        print("ERROR: unknown command " + "'" + op + "'" )
+        input("Press enter to continue...")
+        cls()
+
