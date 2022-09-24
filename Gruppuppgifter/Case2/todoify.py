c67609d7c8022f57e0e981769ebe5f03d7514b37
abbguslof <gustav.lofqvist@abbgymnasiet.se>
2022-09-24 23:49:26 +0200

    fix''

--- Gruppuppgifter/Case2/todoify.py
+++ Gruppuppgifter/Case2/todoify.py
@@ -8,88 +8,88 @@ todo, Completed = [], []
 def PrintTodo():
     i = 0
     for x in todo:
-        print(str(i) + " | " + "[" + Completed[i].rstrip('\n') + "]" + todo[i])
+        print(str(i) + ' | ' + '[' + Completed[i].rstrip('\n') + ']' + todo[i])
         i += 1
 
 def todoSelection(val):
-    if val == "list":
+    if val == 'list':
         i = 0
         for x in todo:
-            print("["+ Completed[i].rstrip("\n") + "]" + todo[i])
+            print('['+ Completed[i].rstrip('\n') + ']' + todo[i])
             i += 1
-        print("-" * 40)
-    elif val == "add":
-        todo.append(input("Todo description > "))
-        Completed.append("O")
-    elif val == "check":
+        print('-' * 40)
+    elif val == 'add':
+        todo.append(input('Todo description > '))
+        Completed.append('O')
+    elif val == 'check':
         PrintTodo()
         try:
-            todoIndex = int(input("Todo index > "))
+            todoIndex = int(input('Todo index > '))
             if 0 <= todoIndex and todoIndex <= len(Completed):
-                if Completed[todoIndex] == "O":
-                    Completed[todoIndex] = "X"
-                    print("-"*40 +"\nUnchecked --> Checked ")
+                if Completed[todoIndex] == 'O':
+                    Completed[todoIndex] = 'X'
+                    print('-'*40 +'\nUnchecked --> Checked ')
                 else:
-                    Completed[todoIndex] = "O"
-                    print("-"*40 + "\nChecked --> Unchecked ")
+                    Completed[todoIndex] = 'O'
+                    print('-'*40 + '\nChecked --> Unchecked ')
             else:
-                print("ERROR: invalid index")
+                print('ERROR: invalid index')
         except ValueError:
-            print("ERROR: invalid index")
-    elif val == "delete":
+            print('ERROR: invalid index')
+    elif val == 'delete':
         PrintTodo()
         try:
-            deleteTodo = int(input("Todo index > "))
+            deleteTodo = int(input('Todo index > '))
             if 0 <= deleteTodo and deleteTodo <= len(todo):
-                print("Deleted " + todo[deleteTodo] + " from your list")
+                print('Deleted ' + todo[deleteTodo] + ' from your list')
                 todo.pop(deleteTodo)
                 Completed.pop(deleteTodo)
             else: 
-                print("ERROR: invalid index")
+                print('ERROR: invalid index')
         except ValueError:
-            print("ERROR: invalid index")
+            print('ERROR: invalid index')
 
 def loadFile(filePath):
         try:
-            with open(filePath, encoding="utf-8") as f:
+            with open(filePath, encoding='utf-8') as f:
                 i = 0
                 for row in f:
-                    loadedTodo = row.split(",")
+                    loadedTodo = row.split(',')
                     todo.append(loadedTodo[0])
                     Completed.append(loadedTodo[1])
         except FileNotFoundError:
-            print("-"*20 + "\nERROR: File is not found")
+            print('-'*20 + '\nERROR: File is not found')
 def saveFile():
-    with open("Todo.csv", "w") as f:
+    with open('Todo.csv', 'w') as f:
         i = 0
         for x in todo:
-            f.write(todo[i] + "," + Completed[i] + "\n")
+            f.write(todo[i] + ',' + Completed[i] + '\n')
             i += 1
 
 while True:
     cls()
-    print("*" * 40)
-    print("Todoify".center(40, " "))
-    print("-" * 40)
-    print("list   | List todos")
-    print("add    | Add todo")
-    print("check  | Check todo")
-    print("delete | Delete todo")
-    print("-" * 40)
-    print("save   | Save todos to file")
-    print("load   | Load todos from file")
-    print("-" * 40)
-    select = input("Selection > ").lower()
-    if select == "list" or select == "add" or select == "check" or select == "delete":
+    print('*' * 40)
+    print('Todoify'.center(40, ' '))
+    print('-' * 40)
+    print('list   | List todos')
+    print('add    | Add todo')
+    print('check  | Check todo')
+    print('delete | Delete todo')
+    print('-' * 40)
+    print('save   | Save todos to file')
+    print('load   | Load todos from file')
+    print('-' * 40)
+    select = input('Selection > ').lower()
+    if select == 'list' or select == 'add' or select == 'check' or select == 'delete':
         todoSelection(select)
-        input("press enter to continue")
-    elif select == "save":
+        input('press enter to continue...')
+    elif select == 'save':
         saveFile()
-        print("-"*40 + "\n" +"Succesfully saved your file as Todo.csv\n")
-        input("press enter to continue...")
-    elif select == "load":
-        loadFile(input("file name > "))
-        input("Press enter to continue...")
+        print('-'*40 + '\n' +'Succesfully saved your file as Todo.csv\n')
+        input('press enter to continue...')
+    elif select == 'load':
+        loadFile(input('file name > '))
+        input('Press enter to continue...')
     else:
-        print("ERROR: Unknown command")
-        input("Press enter to continue...")
+        print('ERROR: Unknown command')
+        input('Press enter to continue...')
